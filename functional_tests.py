3ada7c285b9fa8b2ac051752c75c1d4d8b0edd47
cjmochrie <cameron.mochrie@gmail.com>
2015-10-26 16:45:49 -0400

    Real test

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,25 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome('C:\Program Files\chromedriver\chromedriver')
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Chrome('C:\Program Files\chromedriver\chromedriver')
+        self.browser.implicitly_wait(3)
 
-print('done tests')
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Check out the app on its homepage
+        self.browser.get('http://localhost:8000')
+
+        # The reader notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the Test!')
+
+        # Bunch more narrative
+
+
+if __name__ == '__main__':
+    unittest.main()
