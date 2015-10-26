473d1f900a00421f692098ee066332206a7d8641
cjmochrie <cameron.mochrie@gmail.com>
2015-10-26 19:24:22 -0400

    Refactor functional test for starting list and checking items

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Check out the app on its homepage
         self.browser.get('http://localhost:8000')
@@ -31,21 +36,15 @@ class NewVisitorTest(unittest.TestCase):
         # After hitting enter, the page updates and now the page lists
         # "1. buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy peacock feathers', [row.text for row in rows],
-                        "New to-do item did not appear in table - its text was: \n{}".
-                        format(table.text))
+        self.check_for_row_in_list_table('1: buy peacock feathers')
 
-        # User can add another item to the todo list
+        # User can add another item to the to-do list
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: buy peacock feathers')
+        self.check_for_row_in_list_table('2: use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
