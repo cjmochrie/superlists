70ac602a11d13645c872293812e9f21f8c044d7b
cjmochrie <cameron.mochrie@gmail.com>
2015-10-27 16:45:04 -0400

    Tests passing. Lists work, basic functionality

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,20 @@ from .models import Item, ToDoList
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+
+
+def view_list(request, list_id):
+    todo_list = ToDoList.objects.get(id=list_id)
+    return render(request, 'list.html', {'todo_list': todo_list})
+
 
 def new_list(request):
+
     list_ = ToDoList.objects.create()
-    items = Item.objects.create(text=request.POST['item_text'], todo_list=list_)
-    return redirect('/lists/the-only-list-in-the-world/')
+    Item.objects.create(text=request.POST['item_text'], todo_list=list_)
+    return redirect('/lists/{}/'.format(list_.id))
+
+def add_item(request, list_id):
+    todo_list = ToDoList.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], todo_list=todo_list)
+    return redirect('/lists/{}/'.format(todo_list.id))
