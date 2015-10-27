34b1021184e9f8326e788fb786244d34c1ccf881
cjmochrie <cameron.mochrie@gmail.com>
2015-10-27 17:20:37 -0400

    Add test for centered input box on homepage

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -77,3 +77,13 @@ class NewVisitorTest(LiveServerTestCase):
 
 
         self.fail('Finish the test!')
+
+    def test_layout_and_styling(self):
+        # User goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # User sees a nicely centered input box
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2,
+                               512, delta=5)
