c4127ac41b369d79ac1047269e7244a814af9301
cjmochrie <cameron.mochrie@gmail.com>
2015-10-28 10:19:04 -0400

    Change functional test runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,25 @@
+import sys
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Chrome('C:\Program Files\chromedriver\chromedriver')
@@ -18,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Check out the app on its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # The reader notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -54,7 +70,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Chrome('C:\Program Files\chromedriver\chromedriver')
 
         # Francis visits the home page. No sign of Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('buy peacock feathers', page_text)
         self.assertNotIn('use peacock feathers to make a fly', page_text)
@@ -80,7 +96,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # User goes to the homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # User sees a nicely centered input box
