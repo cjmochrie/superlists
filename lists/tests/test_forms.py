03c4b3a91b6b7937928a71fad5b0f5ba9583c293
cjmochrie <cameron.mochrie@gmail.com>
2015-11-03 17:05:32 -0500

    New ExistingListItemForm

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, \
+    ExistingListItemForm
 from lists.models import Item, ToDoList
 
 class ItemFormTest(TestCase):
@@ -30,6 +31,26 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item.text, 'foobar')
         self.assertEqual(new_item.todo_list, list_)
 
-
     def home(self):
         return self.client.get('/')
+
+
+class ExistingListItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        list_ = ToDoList.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+    def test_form_validation_for_for_blank_items(self):
+        list_ = ToDoList.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = ToDoList.objects.create()
+        Item.objects.create(todo_list=list_, text='no twins!')
+        form =ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.core.urlresolvers import resolve
 from django.template.loader import render_to_string
 from django.http import HttpRequest
@@ -6,7 +8,7 @@ from django.utils.html import escape
 
 from lists.models import Item, ToDoList
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
 
 
 class HomePageTest(TestCase):
@@ -96,6 +98,16 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = ToDoList.objects.create()
+        item1 = Item.objects.create(todo_list=list1, text='textey')
+        response = self.client.post('/lists/{}'.format(list1.id), data={'text': 'textey'})
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
     def test_displays_item_form(self):
         list_ = ToDoList.objects.create()
