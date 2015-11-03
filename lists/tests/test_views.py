55e2dc1aeea5c58a87372a4fe5f444c199c044df
cjmochrie <cameron.mochrie@gmail.com>
2015-11-03 17:25:52 -0500

    Django form magic

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,7 +8,8 @@ from django.utils.html import escape
 
 from lists.models import Item, ToDoList
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, \
+    ExistingListItemForm
 
 
 class HomePageTest(TestCase):
@@ -93,17 +94,16 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = ToDoList.objects.create()
         item1 = Item.objects.create(todo_list=list1, text='textey')
-        response = self.client.post('/lists/{}'.format(list1.id), data={'text': 'textey'})
+        response = self.client.post('/lists/{}/'.format(list1.id), data={'text': 'textey'})
         expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
@@ -112,7 +112,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = ToDoList.objects.create()
         response = self.client.get('/lists/{}/'.format(list_.id))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 
--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 
 from .models import Item, ToDoList
-from .forms import ItemForm
+from .forms import ItemForm, ExistingListItemForm
 
 
 def home_page(request):
@@ -11,10 +11,10 @@ def home_page(request):
 
 def view_list(request, list_id):
     todo_list = ToDoList.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=todo_list)
 
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=todo_list, data=request.POST)
         if form.is_valid():
             form.save(for_list=todo_list)
             return redirect(todo_list)
